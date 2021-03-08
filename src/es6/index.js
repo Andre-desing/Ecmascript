ea1bbbe43f2fb809764b51cf498eaf9615ba4f53
Andrés Miranda <andres.miranda_@outlook.com>
2021-03-07 20:59:06 -0500

    Se realizaron los ejercicios vistos en la clase sobre LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -21,4 +21,61 @@ console.log(epicPhrase);
 let epicPhrase2 =   `${hello} ${world}`;
 console.log(epicPhrase2);
 
+ //Antes de  ES6 era así
+
+ let lorem ="This is an epic quote \n"
+ + "this is another epic quote we'll need."
+
+ //Así es despues de ES6 
+
+ let lorem2 = `another epic quote we'll need
+now this is another epic quote
+ `;
+
+console.log(lorem);
+console.log(lorem2); 
+
+//before es6
+
+let person = {
+    'name' : 'oscar',
+    'age' : 32,
+    'country' : 'MX'
+}
+
+console.log(person.name, person. age);
+
+// after es6
+
+let { name}= person;
+console.log(name);
+
+// before
+
+let team1 = ['Oscar', 'Julian', 'Ricardo']
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+// after
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+// uso de let
+
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = 'Global let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+//It's been added also conts so that we make sure some values remain the same
+
+const a = 'b';
+a = 'a';
  
