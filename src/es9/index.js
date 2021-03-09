33e13a033bf97d9ec2e35447ac93b6b6e297ae50
Andrés Miranda <andres.miranda_@outlook.com>
2021-03-08 23:11:51 -0500

    Se realizan los ejercicios con trim vistos en la clase 10 sobre ES10

--- src/es9/index.js
+++ src/es9/index.js
@@ -25,7 +25,7 @@ console.log(obj1);
 const helloWorld = () => {
     return new Promise((resolve, reject) => {
     (true)
-    ? resolve('hello World')
+    ? setTimeout(() => resolve('hello World'), 3000)
     : reject(new Error('Test Error'))
     });
 };
@@ -34,3 +34,12 @@ helloWorld()
     .then(response => console.log(response))
     .catch(error => console.log(error))
     .finally(() => console.log('Finalizó'))
+
+
+    const regexData = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+    const match = regexData.exec('2018-04-20');
+    const year = match [1]
+    const month = match [2]
+    const day = match [3]
+
+    console.log(year, month, day);
